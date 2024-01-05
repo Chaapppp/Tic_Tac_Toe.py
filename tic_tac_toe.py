29b9955a4cc50d0e012d6881f9d6b9d09b4d7177
Chaapppp <chanisarappp@gmail.com>
2024-01-05 20:10:32 +0700

    Add File

---
+++ tic_tac_toe.py
@@ -0,0 +1,91 @@
+import kivy
+from kivy.app import App
+from kivy.uix.modalview import ModalView
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from copy import deepcopy
+from enum import Enum
+from math import inf
+from functools import partial
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+Builder.load_string(
+
+    <Cell@Button>:
+    background_color: 102 / 255, 102 / 255, 102 / 255, 0.5
+    font_size: 144
+    background_normal: ''
+    background_down: ''
+
+<MainMenu@Screen>:
+    name: 'home'
+    canvas:
+        Color:
+            rgba: 0, 153 / 255, 102 / 255, 0.7
+        Rectangle:
+            size: root.size
+            pos: root.pos
+    Label:
+        size_hint: None, None
+        text: "[color=075951]Welcome to my Tic-Tac-Toe game![/color]\\n\\n           [color=2040a3]Let's have some FUN[/color] [color=b79a00]:)[/color]"
+        markup: True
+        bold: True
+        #color: 0, 0, 0, 1
+        font_size: 43
+        size: self.texture_size
+        pos: ((root.width / 2) - (self.width/ 2)), ((root.height / 2) - (self.height / 2)) + 100
+
+    GridLayout:
+        size_hint: None, None
+        size: root.width * 0.9, 100
+        pos: ((root.width / 2) - (self.width/ 2)), 175
+        spacing: 10
+        cols: 3
+        
+        Button:
+            text: 'Two Players'
+            background_normal: ""
+            background_color: 0,61/255,153/255, 1
+            font_size: 35
+            markup: True
+            bold: True
+            color: 1, 1, 1, 1
+            outline_color: (0, 0, 0)
+            outline_width: 4
+            on_release: app.root.current = 'mp'
+        
+        Button:
+            text: 'One Player'
+            background_normal: ""
+            background_color: 0,61/255,153/255, 1
+            font_size: 35
+            markup: True
+            bold: True
+            color: 1, 1, 1, 1
+            outline_color: (0, 0, 0)
+            outline_width: 4
+            #color: 0, 153 / 255, 102 / 255, 0.7
+            on_release: app.root.current = 'sp'
+        
+        Button:
+            text: 'Exit'
+            background_normal: ""
+            background_color: 0,61/255,153/255, 1
+            font_size: 35
+            markup: True
+            bold: True
+            color: 1, 1, 1, 1
+            outline_color: (0, 0, 0)
+            outline_width: 4
+            on_release: quit()
+
+
+)
