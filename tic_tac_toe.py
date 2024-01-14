4382a1246d8689b453992434d40e59bbc36eb71f
Chaapppp <chanisarappp@gmail.com>
2024-01-14 22:01:32 +0700

    Update class SimpleBoard

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -271,14 +271,16 @@ class MainMenu(Screen):
 class PlayMenu(Screen):
     pass
 
+
 class Player(Enum):
     COMPUTER = "O"
     HUMAN = "X"
     EMPTY = ""
 
+
 class SimpleBoard:
 
-    MAX_SCORE = 10000
+    MAX_SCORE = 100
 
     def __init__(self, board):
         self.__board = [[button.text for button in row] for row in board]
@@ -300,6 +302,7 @@ class SimpleBoard:
     def has_won(self):
         return abs(evaluate(self)) == SimpleBoard.MAX_SCORE
 
+
 def get_possibilities(board, symbol):
     """
     :param board:   The board to insert :symbol: into
@@ -317,11 +320,12 @@ def get_possibilities(board, symbol):
                 out.append((option, (i, j)))
     return out
 
+
 def evaluate(board):
     """
     :param board:   The board to evaluate
     :return:        :board:'s score based on the number of 2 in a rows
-    """ 
+    """
     lines = check_rows(board) + check_cols(board) + check_diags(board)
     two_in_row = [0, 0]
     for line in lines:
@@ -334,6 +338,7 @@ def evaluate(board):
     player_score = 1.5 * 10 * two_in_row[1]
     return comp_score - player_score
 
+
 def check_rows(board):
     """
     :param board:   The game board or a list of rows
@@ -344,6 +349,7 @@ def check_rows(board):
         out.append((row.count(Player.COMPUTER.value), row.count(Player.HUMAN.value)))
     return out
 
+
 def check_cols(board):
     """
     :param board:   The game board
@@ -351,6 +357,7 @@ def check_cols(board):
     """
     transpose = [[row[i] for row in board] for i in range(len(board))]
     return check_rows(transpose)
+
 
 def check_diags(board):
     """
@@ -362,6 +369,7 @@ def check_diags(board):
         [board[i][len(board) - i - 1] for i in range(len(board))],
     ]
     return check_rows(diagonals)
+
 
 def minimax(board, depth):
     """
@@ -375,6 +383,7 @@ def minimax(board, depth):
         return pick_highest(board)
     return make_move(board, Player.COMPUTER, alpha, beta, depth, depth)
 
+
 def pick_highest(board):
     """
     :param board:   The current gamestate
@@ -383,6 +392,7 @@ def pick_highest(board):
     options = get_possibilities(board, Player.COMPUTER.value)
     scores = [evaluate(x[0]) for x in options]
     return options[scores.index(max(scores))][1]
+
 
 def make_move(board, player, alpha, beta, depth, idepth):
     """
@@ -417,6 +427,7 @@ def make_move(board, player, alpha, beta, depth, idepth):
             break
     return best_score if depth != idepth else best_index
 
+
 def better_move(player, score, best_score):
     """
     :param player:      Tells the computer if looking for min or max scores (str, Player.HUMAN/Player.COMPUTER)
@@ -426,31 +437,36 @@ def better_move(player, score, best_score):
     """
     return score > best_score if player == Player.COMPUTER else score < best_score
 
+
 class GameMode(Enum):
     SINGLE_PLAYER = 0
     MULTI_PLAYER = 1
+    
+
+class Color(Enum):
+    O = (32 / 255, 64 / 255, 163 / 255, 1)  # The color for the O's buttons
+    X = (0.2, 0.8, 0.4, 1)  # The color for the X's buttons
+
+    # In Single Player Mode:
+    # COMPUTER = "O"
+    # HUMAN = "X"
+
+
+X, D, O = 0, 0, 0  # global variables for the scores
 
 class Board(GridLayout):
     LENGTH = 3
-    DIFFICULTY = {
-        "baby": 0,
-        "easy": 2,
-        "medium": 4,
-        "hard": 6,
-        "impossible": LENGTH ** 2,
-    }
 
+    DIFFICULTY = {
+        "hard": 6,
+    } 
+    
     def __init__(self, **kwargs):
         super().__init__()
 
         self.cols = self.rows = Board.LENGTH
         self.spacing = 2, 2
-        self.click_sound = SoundLoader.load(os.path.join("assets", "click.wav"))
-        self.game_start = SoundLoader.load(os.path.join("assets", "app_start.wav"))
-        self.new_game_starting = SoundLoader.load(os.path.join("assets", "start.wav"))
-        self.win_game = SoundLoader.load(os.path.join("assets", "win.mp3"))
-        self.lose_game = SoundLoader.load(os.path.join("assets", "lose.wav"))
-
+        
         self.first_player = self.current_player = kwargs.get(
             "first_player", Player.HUMAN
         )
@@ -460,15 +476,24 @@ class Board(GridLayout):
             [Cell() for _ in range(Board.LENGTH)] for _ in range(Board.LENGTH)
         ]
         self.popup = None
-        if self.game_start:
-            self.game_start.play()
+
         self.init_buttons()
         self.first_move()
 
-    def init_buttons(self, reset=False):
+    def click_reset(self, reset=True):
+        print('\n=======\nRestart\n=======\n')
 
-        if self.new_game_starting:  
-            self.new_game_starting.play()  
+    def init_buttons(self, reset=False):
+        """
+        Initialises/resets the button objects in self.button_list by doing the following:
+        - Binding the on_click function
+        - Setting the buttons text value to a blank string  (On reset)
+        - Adding the button to the Board                    (On init)
+
+        Also, create the reset widgets in the board - the 3 button in bottom - Reset, Back and Exit and the ScoreBoard in top
+        :param reset:   Whether to reset or initialise the buttons
+        :return:        None
+        """
 
         board = BoxLayout(orientation="vertical")
         grid = GridLayout(cols=3, rows=3, spacing=2)
@@ -483,7 +508,66 @@ class Board(GridLayout):
 
         grid.pos_hint = {"x": 0.003, "y": 0}
 
-def exitPopup(self, obj):  # The exit popup and its buttons
+        if not reset:
+            self.restart = Button(
+                text="[color=#009966]Restart[/color]",
+                font_size=35,
+                size_hint=(1, 1),
+                on_press=self.click_reset,
+                on_release=self.reset,
+                bold=True,
+                background_color=(0, 0.4, 1, 1),
+                markup=True,
+            )
+
+            # Creating the score board text label and design it
+            self.scoreboard = Label(
+                text="[color=2040a3]Score Board:[/color]\n[color=000000]  [color=145128]X[/color]: 0 – 0 :[color=102e87]O[/color][/color]\n        [color=000000]D: 0[/color]",
+                font_size=35,
+                bold=True,
+                markup=True,
+            )
+
+            # Creating the exit button and design it. also, call in realse click on the button to the exit function
+            self.exit = Button(
+                text="[color=#009966]Exit[/color]",
+                font_size=35,
+                size_hint=(1, 1),
+                on_release=self.exitPopup,
+                bold=True,
+                background_color=(0, 0.4, 1, 1),
+                markup=True,
+            )
+
+            self.back = Button(
+                text="[color=#009966]Back[/color]",
+                font_size=35,
+                size_hint=(1, 1),
+                on_release=lambda *args: self.goto_menu(),
+                bold=True,
+                background_color=(0, 0.4, 1, 1),
+                markup=True,
+            )
+
+            buttons = BoxLayout(orientation="horizontal", padding=[0, 2, 0, 0])
+
+            buttons.add_widget(self.restart)  # Add the reset widget to the board
+            buttons.add_widget(self.back)
+            buttons.add_widget(self.exit)  # Add the exir widget to the board
+
+            buttons.size_hint = (1.003, 0.3)
+            buttons.pos_hint = {"x": 0.001}
+
+            self.scoreboard.pos_hint = {"top": 0.8}
+            self.scoreboard.size_hint = (1, 0.4)
+
+            board.add_widget(self.scoreboard)  # Add the score board widget to the board
+
+            board.add_widget(grid)
+            board.add_widget(buttons)
+            self.add_widget(board)
+
+    def exitPopup(self, obj):  # The exit popup and its buttons
         self.box_popup = BoxLayout(
             orientation="horizontal"
         )  # Create a box layout fot the exit popup
@@ -495,24 +579,6 @@ def exitPopup(self, obj):  # The exit popup and its buttons
             content=self.box_popup,
             size_hint=(0.5, 0.4),
             auto_dismiss=True,
-        )
-
-        # Add for it text and design and reposition it
-        self.box_popup.add_widget(
-            Label(
-                text="                               Are you sure you want to exit?",
-                font_size=22,
-                pos_hint={"x": 0, "y": 0.1},
-            )
-        )
-
-        self.box_popup.add_widget(
-            Button(
-                text="Yes",
-                on_release=self.bye,
-                size_hint=(0.45, 0.2),
-                background_color=(1, 0, 0, 1),
-            )
         )
 
         self.box_popup.add_widget(
